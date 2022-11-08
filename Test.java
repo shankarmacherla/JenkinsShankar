3a5722f59d89516f748b748f44fba32f72738f09
Saitejavanumu1234 <saitejavanumu0@gmail.com>
2022-11-08 10:28:42 +0530

    file updated my shankar macherla

---
+++ Test.java
@@ -0,0 +1,5 @@
+public class Test{
+     public static void main(String[] args){
+                 System.out.println("This is typed in Notepad by Shankar Macherla in local git and pushed to git hub and run through jenkins ");
+     }
+}
